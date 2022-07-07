a0163e1f8e309ff558f9929f679a73af0a868b22
Tester5555 <sarah_li5@hotmail.com>
2022-07-07 11:20:28 +1000

    Support empty input and multiple input

--- RecommCourses.py
+++ RecommCourses.py
@@ -16,8 +16,10 @@ def recommCourses():
 	if request.method == 'POST':
 		session.permanent = True
 		skills = request.form['skills']
-		
-		return redirect(url_for('courses', skills=skills))
+		if skills:
+			return redirect(url_for('courses', skills=skills))
+		else: 
+			return render_template('recommCourses.html')
 	else: 
 		return render_template('recommCourses.html')
 
@@ -25,25 +27,25 @@ def recommCourses():
 def courses(skills):
 	msg = 'Coursera provides the following courses related to "' + skills + '" :'
 	s_list = list(map(str.strip, skills.split(',')))
+	skill_courses = []
 
 	for skill in s_list:
 		url_coursera = 'https://api.coursera.org/api/courses.v1?q=search&query=' + skill
-		courses_list = listcourses(url_coursera, skill)
+		courses_list = listcourses(url_coursera, skill, skill_courses)
 		
 	return  render_template('courses.html', msg=msg, courses_list=courses_list)
 
-def listcourses(url, skill):
+def listcourses(url, skill, skill_courses):
 	res = requests.get(url)
 	courses = json.loads(res.text)
-	courses_list = []
-	
+		
 	for course in courses['elements']:
 		course_name = course['name']
 		course_url = 'https://www.coursera.org/learn/' + course['slug']
 		course_dic = {'name': course_name, 'url': course_url}
-		courses_list.append(course_dic)
+		skill_courses.append(course_dic)
 		
-	return courses_list
+	return skill_courses
 
 if __name__ == '__main__':
 	app.run(debug=True)
